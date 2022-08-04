9d0f5a20504111456d4f8479b45c85f3810525f9
Minh Tan <MinhTan@Apples-MacBook-Pro.local>
2022-08-04 21:23:00 +0700

    add code

--- src/index.js
+++ src/index.js
@@ -17,10 +17,6 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getPerformance } from "firebase/performance";
 import './firebase';
-import {
-  getPerformance,
-  trace
-} from 'firebase/performance';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
