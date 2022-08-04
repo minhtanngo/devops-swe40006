4ba802008d33f5085b5f76589c382b34b2480246
Minh Tan <MinhTan@Apples-MacBook-Pro.local>
2022-08-04 18:08:50 +0700

    Add monitoring

--- src/index.js
+++ src/index.js
@@ -12,29 +12,7 @@ root.render(
   </React.StrictMode>
 );
 
-// Import the functions you need from the SDKs you need
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyADv6DUnpjQK125q2vG2CHSFVFBXybq9g4",
-  authDomain: "devops-swe40006.firebaseapp.com",
-  projectId: "devops-swe40006",
-  storageBucket: "devops-swe40006.appspot.com",
-  messagingSenderId: "1027839023764",
-  appId: "1:1027839023764:web:12661cea003ba8205ef281",
-  measurementId: "G-N9C5K5LSV7"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const perf = getPerformance(app);
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-getPerformance();
