a971ed944c416261e608cbcf590b4a19e5a82fc5
Minh Tan <MinhTan@Apples-MacBook-Pro.local>
2022-08-04 21:20:36 +0700

    Add files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './firebase';
+// import './firebase';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,7 +12,36 @@ root.render(
   </React.StrictMode>
 );
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getPerformance } from "firebase/performance";
+import './firebase';
+import {
+  getPerformance,
+  trace
+} from 'firebase/performance';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyADv6DUnpjQK125q2vG2CHSFVFBXybq9g4",
+  authDomain: "devops-swe40006.firebaseapp.com",
+  projectId: "devops-swe40006",
+  storageBucket: "devops-swe40006.appspot.com",
+  messagingSenderId: "1027839023764",
+  appId: "1:1027839023764:web:12661cea003ba8205ef281",
+  measurementId: "G-N9C5K5LSV7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const perf = getPerformance(app);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
