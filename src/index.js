35b7ad7203052a348ea7c4794920b6a46bc3ba7c
Minh Tan <MinhTan@Apples-MacBook-Pro.local>
2022-08-04 21:25:34 +0700

    Add files

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import './firebase';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getPerformance } from "firebase/performance";
+import './firebase';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,12 +16,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getPerformance } from "firebase/performance";
-import './firebase';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
