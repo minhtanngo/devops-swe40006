af616fa75b0cbb7acead6195cd0ded1c7b94ff2a
Minh Tan <MinhTan@Apples-MacBook-Pro.local>
2022-08-04 22:41:33 +0700

    Add files

--- src/firebase.js
+++ src/firebase.js
@@ -1,25 +1,25 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getPerformance } from "firebase/performance";
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import { getPerformance } from "firebase/performance";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyADv6DUnpjQK125q2vG2CHSFVFBXybq9g4",
-  authDomain: "devops-swe40006.firebaseapp.com",
-  projectId: "devops-swe40006",
-  storageBucket: "devops-swe40006.appspot.com",
-  messagingSenderId: "1027839023764",
-  appId: "1:1027839023764:web:12661cea003ba8205ef281",
-  measurementId: "G-N9C5K5LSV7"
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyADv6DUnpjQK125q2vG2CHSFVFBXybq9g4",
+//   authDomain: "devops-swe40006.firebaseapp.com",
+//   projectId: "devops-swe40006",
+//   storageBucket: "devops-swe40006.appspot.com",
+//   messagingSenderId: "1027839023764",
+//   appId: "1:1027839023764:web:12661cea003ba8205ef281",
+//   measurementId: "G-N9C5K5LSV7"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
-const analytics = getAnalytics(app);
-const perf = getPerformance(app);
+// const analytics = getAnalytics(app);
+// const perf = getPerformance(app);
